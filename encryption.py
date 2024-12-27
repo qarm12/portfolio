2f1e5cbd99c9d0ed0f83e5a84807e5e00c5b0ba5
Quinn Armstead <45981482+qarm12@users.noreply.github.com>
2024-12-27 10:23:24 -0800

    ...

--- encryption.py
+++ encryption.py
@@ -4,33 +4,28 @@ plain_text = input('Enter plaintext: ')
 cipher_text = ""
 plain_text1 = ""
 
-characters = "1234567890!@#$%^&*()~`_-+={[}]\|qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM<,.>?/:;' "
-char_key = "1234567890!@#$%^&*()~`_-+={[}]\|qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM<,.>?/:;' "
+characters = "1234567890!@#$%^&*()~`_-+={[}]|qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM<,.>?/:;' "
+char_key = "1234567890!@#$%^&*()~`_-+={[}]|qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM<,.>?/:;' "
 
 char_key = list(char_key)
 characters = list(characters)
 
 random.shuffle(char_key)
-#print(characters)
-#print(char_key)
 
 for letter in plain_text:
     index = characters.index(letter)
     cipher_text += char_key[index]
 
-print(f'Encrypted Message: {cipher_text} ')
-
-print('')
-
+print(f'Encrypted Message: {cipher_text}')
 
 for letter in cipher_text:
     index = characters.index(letter)
     plain_text1 += char_key[index]
 
-uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+uppercase_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 lowercase_letters = uppercase_letters.lower()
-digits = "0123456789"
-symbols = "(){}[],:;,-_/\\?+*#"
+digits = '0123456789'
+symbols = '(){}[],:;,-_/?+*#~`><|=!@$%^&'
 
 # Decides Inclusive Characters
 upper, lower, nums, syms = True, True, True, True
@@ -49,8 +44,8 @@ if syms:
   all += symbols
 
 # Setting Parameters for Password
-length = 20
-amount = 10
+length = 50
+#amount = 10
 
 # Execution
 password = "".join(random.sample(all, length))
@@ -62,4 +57,4 @@ print('')
 if key == password:
     print(f'Decrypted Message: {plain_text}')
 else:
-    print("This message isn't for your eyes to see. ")
+    print('Incorrect key.')
