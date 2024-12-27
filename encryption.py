ce85ebd6324c85d86f10827a758e0858eecb1666
Quinn Armstead <45981482+qarm12@users.noreply.github.com>
2024-12-26 21:23:00 -0800

    ...

---
+++ encryption.py
@@ -0,0 +1,65 @@
+import random
+
+plain_text = input('Enter plaintext: ')
+cipher_text = ""
+plain_text1 = ""
+
+characters = "1234567890!@#$%^&*()~`_-+={[}]\|qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM<,.>?/:;' "
+char_key = "1234567890!@#$%^&*()~`_-+={[}]\|qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM<,.>?/:;' "
+
+char_key = list(char_key)
+characters = list(characters)
+
+random.shuffle(char_key)
+#print(characters)
+#print(char_key)
+
+for letter in plain_text:
+    index = characters.index(letter)
+    cipher_text += char_key[index]
+
+print(f'Encrypted Message: {cipher_text} ')
+
+print('')
+
+
+for letter in cipher_text:
+    index = characters.index(letter)
+    plain_text1 += char_key[index]
+
+uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lowercase_letters = uppercase_letters.lower()
+digits = "0123456789"
+symbols = "(){}[],:;,-_/\\?+*#"
+
+# Decides Inclusive Characters
+upper, lower, nums, syms = True, True, True, True
+
+# Password String
+all = ""
+
+# Configuring Random Password
+if upper:
+  all += uppercase_letters
+if lower:
+  all += lowercase_letters
+if nums:
+  all += digits
+if syms:
+  all += symbols
+
+# Setting Parameters for Password
+length = 20
+amount = 10
+
+# Execution
+password = "".join(random.sample(all, length))
+print('')
+print(f"Key: {password}")
+
+key = input('Enter key: ')
+print('')
+if key == password:
+    print(f'Decrypted Message: {plain_text}')
+else:
+    print("This message isn't for your eyes to see. ")
