11c5b112482a0f25485c1ee695ff84ce2a682de5
Quinn Armstead <45981482+qarm12@users.noreply.github.com>
2024-12-26 21:18:49 -0800

    ...

---
+++ password_generator.py
@@ -0,0 +1,31 @@
+import random
+
+# Setting Possible Options
+uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lowercase_letters = uppercase_letters.lower()
+digits = "0123456789"
+symbols = "(){}[],:;,-_/\\?+*#"
+
+# Decides Inclusive Characters
+upper, lower, nums, syms = True, True, True, True
+
+# Password String
+all = ""
+
+# Configuring Random Password
+if upper:
+  all += uppercase_letters
+if lower:
+  all += lowercase_letters
+if nums:
+  all += digits
+if syms:
+  all += symbols
+
+# Setting Parameters for Password
+length = 20
+amount = 10
+
+# Execution
+password = "".join(random.sample(all, length))
+print(password)
